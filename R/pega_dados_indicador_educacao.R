8027c1f1e2630998194b723ace980745cb299e5b
adelino-dev <adelino.developer@gmail.com>
2025-03-29 16:22:47 -0300

    feat: add get_gastos_totais_educacao function

--- R/pega_dados_indicador_educacao.R
+++ R/pega_dados_indicador_educacao.R
@@ -430,3 +430,64 @@ get_taxa_liq_matricula_creche <- function(ano = '2023'){
   message('Dados filtrados e processados com sucesso.')
   return(taxa_liq_matricula)
 }
+
+
+#' Obter gastos totais com educação por município
+#'
+#' Esta função carrega e processa os dados de gastos públicos com educação
+#' a partir de arquivos CSV, filtrando por município e consolidando os valores.
+#'
+#' Os dados foram retirados de https://siconfi.tesouro.gov.br/siconfi/pages/public/consulta_finbra/finbra_list.jsf
+#'
+#' @param ano Integer. Ano dos dados a serem carregados (entre 2013 e 2024). Default: 2022.
+#' @return Dataframe com os municípios e seus respectivos gastos totais com educação.
+#' @examples
+#' gastos_educacao <- get_gastos_totais_educacao(ano = 2023)
+#' head(gastos_educacao)
+#'
+#' @importFrom readr read_delim locale
+#' @export
+get_gastos_totais_educacao <- function(ano = 2022) {
+  anos_disponiveis <- as.character(2013:2024)
+  ano <- as.character(ano)
+
+  if (!(ano %in% anos_disponiveis)) {
+    stop("Ano inválido. Os anos disponíveis são: ", paste(anos_disponiveis, collapse = ", "))
+  }
+
+  file_path <- paste0('data_raw/finbra_despesas_por_funcao/finbra_despesas_por_funcao_', ano, '.csv')
+
+  message("Carregando a base de dados...")
+  df <- readr::read_delim(file_path,
+                          delim = ";",
+                          locale = locale(encoding = "ISO-8859-1"),
+                          skip = 3,
+                          show_col_types = FALSE)
+  message("Base de dados carregada com sucesso!")
+
+  message("Filtrando dados...")
+  gastos_totais_edu <- df %>%
+    filter(Conta == '12 - Educação',
+           Coluna %in% c('Despesas Empenhadas', 'Despesas Liquidadas', 'Despesas Pagas'))
+
+  message("Filtrando cidades...")
+  gastos_totais_edu <- gastos_totais_edu %>%
+    mutate(`Cod.IBGE` = as.character(`Cod.IBGE`)) %>%
+    filter(`Cod.IBGE` %in% target_cities$municipio_codigo)
+
+  message("Calculando gastos totais com educação...")
+  gastos_totais_edu <- gastos_totais_edu %>%
+    group_by(`Cod.IBGE`) %>%
+    summarise(gastos_totais_educacao = sum(as.double(gsub(",", ".", Valor))), .groups = 'drop') %>%
+    rename(municipio_codigo = `Cod.IBGE`)
+
+  message("Selecionando colunas...")
+  gastos_totais_edu <- gastos_totais_edu %>%
+    right_join(target_cities, by = 'municipio_codigo') %>%
+    select(municipio_codigo, municipio_nome, estado_sigla, gastos_totais_educacao)
+
+  message("Concluído!")
+  return(gastos_totais_edu)
+}
+
+
