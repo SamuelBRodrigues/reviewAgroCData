3aed4cad405d77311031eed9a4d1950cf4be6712
adelino-dev <adelino.developer@gmail.com>
2025-04-01 09:26:34 -0300

    feat: add pega_dados_indicador_gestao_de_qualidade function

---
+++ R/pega_dados_indicador_gestao_da_qualidade.r
@@ -0,0 +1,22 @@
+
+#' Coleta completa dos Indicadores de Gestão da Qulidade
+#'
+#' Executa o pipeline de extração dos indicadores de gestão da qualidade nos municípios.
+#'
+#' @param ano Ano de referência. Padrão: "2023"
+#' @param download Lógico. Se TRUE, baixa os dados. Padrão: TRUE
+#'
+#' @return Uma lista com os dataframes: endividamento, liquidez, poupanca_corrente.
+#' @export
+pega_dados_indicador_educacao <- function(ano = "2023", download = TRUE) {
+  
+  endividamento <- get_endividamento(download=download, ano = ano)
+  poupanca_corrente <- get_poupanca_corrente(download=FALSE, ano = ano)
+  liquidez <- get_liquidez(download=FALSE, ano = ano)
+
+  return(list(
+    endividamento = endividamento,
+    poupanca_corrente = poupanca_corrente,
+    liquidez = liquidez
+  ))
+}
