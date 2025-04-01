83c9bb7812f60038a59fdf488d6dfe6985d97e30
adelino-dev <adelino.developer@gmail.com>
2025-03-31 23:30:17 -0300

    feat: add get_liquidez function and delete extract_CAPAG_liquidez_2022 function

--- R/get_liquidez.R
+++ R/get_liquidez.R
@@ -1,22 +1,24 @@
-#' Obtém dados municipais de Dívida Consolidada/Receita Corrente Líquida (Endividamento)
+#' Obtém dados municipais de Obrigações Financeiras/Disponibilidade de Caixa (Liquidez)
 #'
-#' Esta função baixa e processa os dados de endividamento municipal a partir dos arquivos CAPAG.
+#' Esta função baixa e processa os dados de liquidez municipal a partir dos arquivos CAPAG.
 #'
 #' @param download Lógico. Se `TRUE`, baixa o arquivo correspondente ao ano escolhido. Default é `TRUE`.
 #' @param ano Inteiro ou caractere. Ano desejado para a obtenção dos dados. Valores válidos: 2017 a 2023. Default é 2023.
 #'
-#' @return Um `data.frame` contendo os dados de endividamento dos municípios para o ano especificado, incluindo os códigos dos municípios, nomes e estados.
+#' @return Um `data.frame` contendo os dados de liquidez dos municípios para o ano especificado, incluindo os códigos dos municípios, nomes e estados.
 #'
 #' @examples
 #'
 #' # Obtendo dados para 2023 (sem baixar novamente se o arquivo já existir)
-#' df <- get_endividamento(download = FALSE, ano = 2023)
+#' df <- get_liquidez(download = FALSE, ano = 2023)
 #'
-#' # Obtendo dados para 2021, forçando o download
-#' df <- get_endividamento(download = TRUE, ano = 2021)
+#' # Obtendo dados para 2021 e assegurando a realização do download
+#' df <- get_liquidez(download = TRUE, ano = 2021)
 #'
 #' @export
-get_endividamento <- function(download = TRUE, ano = 2023){
+get_liquidez <- function(download = TRUE, ano = 2023) {...}
+
+get_liquidez <- function(download = TRUE, ano = 2023){
   anos_disponiveis <- as.character(2017:2023)
   ano <- as.character(ano)
 
@@ -57,44 +59,44 @@ get_endividamento <- function(download = TRUE, ano = 2023){
 
   message('Filtering and selecting relevant columns...')
   if (ano == '2021'){
-    endividamento_data <- capag_data %>%
+    liquidez_data <- capag_data %>%
       filter(Ano_Base== '2021') %>%
-      mutate(endividamento = coalesce(as.numeric(Indicador_1_Revisão), as.numeric(Indicador_1))) %>%
+      mutate(liquidez = coalesce(as.numeric(Indicador_3_Revisão), as.numeric(Indicador_3))) %>%
       select(`Cod.IBGE`,
-             `endividamento`) %>%
+             `liquidez`) %>%
       rename(`municipio_codigo` = `Cod.IBGE`,
-             !!paste0('endividamento_',ano) := `endividamento`)
+             !!paste0('liquidez_',ano) := `liquidez`)
 
   } else if (ano == '2023'){
 
-    endividamento_data <- capag_data %>%
+    liquidez_data <- capag_data %>%
       select(`Código Município Completo`,
-             `Indicador 1`) %>%
-     rename(`municipio_codigo` = `Código Município Completo`,
-            !!paste0('endividamento_',ano) := `Indicador 1`)
+             `Indicador 3`) %>%
+      rename(`municipio_codigo` = `Código Município Completo`,
+             !!paste0('liquidez_',ano) := `Indicador 3`)
   } else if (ano == '2018'){
-    endividamento_data <- capag_data %>%
+    liquidez_data <- capag_data %>%
       select(`Cod.IBGE`,
-             `Indicador 1`) %>%
+             `Indicador 3`) %>%
       rename(`municipio_codigo` = `Cod.IBGE`,
-             !!paste0('endividamento_',ano) := `Indicador 1`)
+             !!paste0('liquidez_',ano) := `Indicador 3`)
   } else{
-    endividamento_data <- capag_data %>%
+    liquidez_data <- capag_data %>%
       select(`Cod.IBGE`,
-             `Indicador_1`) %>%
+             `Indicador_3`) %>%
       rename(`municipio_codigo` = `Cod.IBGE`,
-             !!paste0('endividamento_',ano) := `Indicador_1`)
+             !!paste0('liquidez_',ano) := `Indicador_3`)
   }
 
   message('Filtering data for target cities...')
-  endividamento_data <- endividamento_data %>%
+  liquidez_data <- liquidez_data %>%
     mutate(municipio_codigo = as.character(municipio_codigo)) %>%
     right_join(target_cities, by='municipio_codigo') %>%
     select(municipio_codigo,
            municipio_nome,
            estado_sigla,
-           !!paste0('endividamento_', ano))
+           !!paste0('liquidez_', ano))
 
   message('Data filtered and processed successfully.')
-  return(endividamento_data)
+  return(liquidez_data)
 }
